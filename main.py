89d055b7098ba5d936e2c5d73b6e7252a1980da3
Francisco De La Cruz <helloquico@gmail.com>
2025-07-31 21:49:15 +0200

    Application Factory

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-from celery import Celery
-from fastapi import FastAPI
+from project import create_app
 
-app = FastAPI()
-
-celery = Celery(
-    __name__,
-    broker="redis://127.0.0.1:6379/0",
-    backend="redis://127.0.0.1:6379/0",
-)
-
-# Add this to your Celery config to silence the warning
-celery.conf.update(
-    broker_connection_retry_on_startup=True,
-)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello, World!"}
-
-
-@celery.task
-def divide(x, y):
-    import time
-
-    time.sleep(5)
-    return x / y
+app = create_app()

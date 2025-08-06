62798fed4f51330ce2cac97a1ddc4d89632fe1f5
Francisco De La Cruz <helloquico@gmail.com>
2025-08-06 21:26:24 +0200

    Periodic Tasks with Celery Beat

--- project/config.py
+++ project/config.py
@@ -26,6 +26,13 @@ class BaseConfig:
         "WS_MESSAGE_QUEUE", "redis://127.0.0.1:6379/0"
     )
 
+    # beat_schedule = {
+    #     "task-schedule-work": {
+    #         "task": "task_schedule_work",
+    #         "schedule": 5.0,  # five seconds
+    #     },
+    # }
+
 
 class DevelopmentConfig(BaseConfig):
     pass

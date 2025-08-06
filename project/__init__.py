4b5e5ff20221945bb477f3725287ae6da54e0ed7
Francisco De La Cruz <helloquico@gmail.com>
2025-08-06 09:17:48 +0200

    WebSockets and Celery

--- project/__init__.py
+++ project/__init__.py
@@ -1,17 +1,35 @@
+from contextlib import asynccontextmanager
+
+from broadcaster import Broadcast
 from fastapi import FastAPI
 
-from project.celery_utils import create_celery
+from project.config import settings
+
+broadcast = Broadcast(settings.WS_MESSAGE_QUEUE)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await broadcast.connect()
+    yield
+    await broadcast.disconnect()
 
 
 def create_app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
 
     # do this before loading routes
+    from project.celery_utils import create_celery
+
     app.celery_app = create_celery()
 
     from project.users import users_router
 
     app.include_router(users_router)
+
+    from project.ws import ws_router
+
+    app.include_router(ws_router)
 
     @app.get("/")
     async def root():

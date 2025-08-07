72e86cfda754bde32ebbea900b7c6890527b3733
Francisco De La Cruz <helloquico@gmail.com>
2025-08-07 07:26:47 +0200

    Group Celery-related settings

--- project/config.py
+++ project/config.py
@@ -1,7 +1,7 @@
 import os
-from typing import List
 import pathlib
 from functools import lru_cache
+from typing import List
 
 from kombu import Queue
 
@@ -14,20 +14,22 @@ class BaseConfig:
     )
     DATABASE_CONNECT_DICT: dict = {}
 
+    WS_MESSAGE_QUEUE: str = os.environ.get(
+        "WS_MESSAGE_QUEUE", "redis://127.0.0.1:6379/0"
+    )
+
+    # Celery configuration
     broker_url: str = os.environ.get(
         "CELERY_BROKER_URL",
         "redis://127.0.0.1:6379/0",
     )
+
     result_backend: str = os.environ.get(
         "CELERY_RESULT_BACKEND",
         "redis://127.0.0.1:6379/0",
     )
 
     broker_connection_retry_on_startup: bool = True
-
-    WS_MESSAGE_QUEUE: str = os.environ.get(
-        "WS_MESSAGE_QUEUE", "redis://127.0.0.1:6379/0"
-    )
 
     # beat_schedule = {
     #     "task-schedule-work": {

02f67ecdfd59ef6e0d34a94d19102d8c0035929a
Francisco De La Cruz <helloquico@gmail.com>
2025-08-07 21:30:31 +0200

    Database Transactions

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -1,9 +1,12 @@
 import random
+
 import requests
 from asgiref.sync import async_to_sync
 from celery import Task, shared_task
 from celery.signals import task_postrun
 from celery.utils.log import get_task_logger
+
+from project.database import db_context
 
 logger = get_task_logger(__name__)
 
@@ -70,3 +73,12 @@ def dynamic_example_two():
 @shared_task(name="high_priority:dynamic_example_three")
 def dynamic_example_three():
     logger.info("Example Three")
+
+
+@shared_task()
+def task_send_welcome_email(user_pk):
+    from project.users.models import User
+
+    with db_context() as session:
+        user = session.get(User, user_pk)
+        logger.info(f"send email to {user.email} {user.id}")

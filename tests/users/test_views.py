3ae6df339140e769dd86ccfcc6abde55e347c8f1
Francisco De La Cruz <helloquico@gmail.com>
2025-08-09 22:25:33 +0200

    Pytest - Test Solution 2
    dummy data with factory_boy

--- tests/users/test_views.py
+++ tests/users/test_views.py
@@ -2,8 +2,9 @@ from unittest import mock
 
 import requests
 
-from project.users import users_router
+from project.users import users_router, tasks
 from project.users.models import User
+from project.users.factories import UserFactory
 
 
 def test_pytest_setup(client, db_session):
@@ -47,3 +48,25 @@ def test_view_with_eager_mode(client, db_session, settings, monkeypatch):
     mock_requests_post.assert_called_with(
         "https://httpbin.org/delay/5", data={"email": user_email}
     )
+
+
+def test_user_subscribe_view(client, db_session, settings, monkeypatch):
+    user = UserFactory.build()
+
+    task_add_subscribe = mock.MagicMock(name="task_add_subscribe")
+    task_add_subscribe.return_value = mock.MagicMock(task_id="task_id")
+    monkeypatch.setattr(tasks.task_add_subscribe, "delay", task_add_subscribe)
+
+    response = client.post(
+        users_router.url_path_for("user_subscribe"),
+        json={"email": user.email, "username": user.username},
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "send task to Celery successfully",
+    }
+
+    # query from the db again
+    user = db_session.query(User).filter_by(username=user.username).first()
+    task_add_subscribe.assert_called_with(user.id)

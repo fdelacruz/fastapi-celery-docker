8a9ca9da8e316f9b2566423737d6e4b24cd3e3ce
Francisco De La Cruz <helloquico@gmail.com>
2025-08-16 19:58:11 +0200

    Task Decorator
    - Custom Logic
    - Custom Task Decorator

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -7,6 +7,7 @@ from celery import Task, shared_task
 from celery.signals import after_setup_logger, task_postrun
 from celery.utils.log import get_task_logger
 
+from project.celery_utils import custom_celery_task
 from project.database import db_context
 
 logger = get_task_logger(__name__)
@@ -45,7 +46,7 @@ class BaseTaskWithRetry(Task):
     retry_backoff = True
 
 
-@shared_task(bind=True, base=BaseTaskWithRetry)
+@custom_celery_task(max_retries=3)
 def task_process_notification(self):
     if not random.choice([0, 1]):
         # mimic random error
